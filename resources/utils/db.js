2ac664bf6b61a1107acc08fd38b5e1f0b4f1f540
Aditya Pandey <pandey.api2k@gmail.com>
2022-03-27 19:22:56 +0530

    [Init] 1. Added MongoDb connection using Mongoose ORM 2. Added Custom Error and Error handler wrapper 3. Created Server.js file

---
+++ resources/utils/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import SECRETS from "../configs/config.js";
+
+
+export const connect = (url = SECRETS.MONGODB_CONNECTION_STRING) => {
+  return mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+import express, {json } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { connect } from "./resources/utils/db.js";
+import SECRETS from "./resources/configs/config.js";
+const app = express();
+const PORT = SECRETS.PORT || 3000;
+
+
+app.use(json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+const successString = '<div style="display : flex;justify-content:center"><h1><i>Server is Running!</i></h1></div>';
+app.get("/",(req,res)=>res.status(200).send(successString));
+
+
+app.use((err, req, res,next)=>{
+    const { status = 500, message = "Internal Server Error", stack } = err;
+    res.status(status).send({status, message, stack});
+  })
+
+
+const start = async () => {
+try {
+    await connect();
+    app.listen(PORT, () => {
+    if (SECRETS.NODE_ENV === "development") {
+        console.log(`REST API on http://localhost:${PORT}/`);
+    }
+    });
+} catch (err) {
+    console.error(err, "This is the error in starting the server");
+}
+};
+start();
